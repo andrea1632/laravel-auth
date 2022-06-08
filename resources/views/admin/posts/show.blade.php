e75ab83919872f80b20d128ce53a3ea980c36f82
andrea1632 <andrea.bellucci10@libero.it>
2022-06-08 16:14:55 +0200

    added view show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,16 +2,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @foreach ($posts as $post)
-            <div class="col border m-1">
+            <div class="col">
                 <h3>{{$post->title}}</h3>
                 <img src="{{$post->image}}" alt="">
                 <p>{{$post->description}}</p>
-                <div>
-                    <a href="{{route('admin.posts.show', $post->id)}}">View</a>
-                </div>
             </div>
-            @endforeach
         </div>
     </div>
 @endsection
